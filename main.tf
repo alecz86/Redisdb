ccb757f8419486c190e01684439f37a71bb5bc21
alecz86 <alecz86@gmail.com>
2025-02-02 20:19:34 -0500

    edited main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_elasticache_cluster" "redis_db" {
   engine               = "redis"
   node_type            = "cache.m4.large"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
+  parameter_group_name = "default.redis7.0"
   engine_version       = "7.0"
   port                 = 6379
   tags = {
