bbe5106873ecbcaf3aabd3fa248d1c807e6ee63b
alecz86 <alecz86@gmail.com>
2025-02-02 19:53:56 -0500

    added code to files

---
+++ output.tf
@@ -0,0 +1,12 @@
+resource "aws_elasticache_cluster" "redis_db" {
+  cluster_id           = "redis-db"
+  engine               = "redis"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  engine_version       = "3.2.10"
+  port                 = 6379
+  tags = {
+    Name = "DevOps October2024"
+  }
+}
