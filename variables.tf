59d9dc63f858de1f9b2289c321ae0af94effdd15
alecz86 <alecz86@gmail.com>
2025-02-02 21:33:34 -0500

    added tfvars file

--- variables.tf
+++ variables.tf
@@ -1,11 +1,3 @@
-variable "access_key" {
-    description = "Your Aws access key"
-}
-
-variable "secret_key" {
-    description = "Your Secret Key"
-}
-
 variable "region" {
     description = "AWS region"
 }
