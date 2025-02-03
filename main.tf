e372f606a1f8f063e6c0c93ac06057e2fc45bbea
alecz86 <alecz86@gmail.com>
2025-02-02 20:41:34 -0500

    modified main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,6 @@ resource "aws_elasticache_cluster" "redis_db" {
   engine               = "redis"
   node_type            = "cache.m4.large"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.1"
   port                 = 6379
   tags = {
     Name = "DevOps October2024"
